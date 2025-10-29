b3573aec10e4ae2e0f694a3f7ee90b79aa104ab6
Patrick <pfkellogg@gmail.com>
2025-10-29 15:44:37 -0500

    display new meal and workout

--- tracalorie-part-2/js/app.js
+++ tracalorie-part-2/js/app.js
@@ -13,21 +13,19 @@ class CalorieTracker {
     this._displayCaloriesProgress()
   }
 
-  // Public Methods (API)
-
   addMeal(meal) {
     this._meals.push(meal)
     this._totalCalories += meal.calories
+    this._displayNewMeal(meal)
     this._render()
   }
 
   addWorkout(workout) {
     this._workouts.push(workout)
     this._totalCalories -= workout.calories
+    this._displayNewWorkout(workout)
     this._render()
   }
-
-  // Private Methods
 
   _displayCaloriesTotal() {
     const totalCaloriesEl = document.getElementById('calories-total')
@@ -46,6 +44,7 @@ class CalorieTracker {
       (total, meal) => total + meal.calories,
       0
     )
+
     caloriesConsumedEl.innerHTML = consumed
   }
 
@@ -56,28 +55,23 @@ class CalorieTracker {
       (total, workout) => total + workout.calories,
       0
     )
+
     caloriesBurnedEl.innerHTML = burned
   }
 
   _displayCaloriesRemaining() {
     const caloriesRemainingEl = document.getElementById('calories-remaining')
     const progressEl = document.getElementById('calorie-progress')
-
     const remaining = this._calorieLimit - this._totalCalories
     caloriesRemainingEl.innerHTML = remaining
-
     if (remaining <= 0) {
-      caloriesRemainingEl.parentElement.parentElement.classList.remove(
-        'bg-light'
-      )
-      caloriesRemainingEl.parentElement.parentElement.classList.add('bg-danger')
-      progressEl.classList.remove('bg-success')
+      caloriesRemainingEl.parentElement.classList.remove('bg-light')
+      caloriesRemainingEl.parentElement.classList.add('bg-danger')
       progressEl.classList.add('bg-danger')
+      progressEl.classList.remove('bg-success')
     } else {
-      caloriesRemainingEl.parentElement.parentElement.classList.remove(
-        'bg-danger'
-      )
-      caloriesRemainingEl.parentElement.parentElement.classList.add('bg-light')
+      caloriesRemainingEl.parentElement.classList.remove('bg-danger')
+      caloriesRemainingEl.parentElement.classList.add('bg-light')
       progressEl.classList.remove('bg-danger')
       progressEl.classList.add('bg-success')
     }
@@ -88,6 +82,48 @@ class CalorieTracker {
     const percentage = (this._totalCalories / this._calorieLimit) * 100
     const width = Math.min(percentage, 100)
     progressEl.style.width = `${width}%`
+  }
+
+  _displayNewMeal(meal) {
+    const mealsEl = document.getElementById('meal-items')
+    const mealEl = document.createElement('div')
+    mealEl.classList.add('card', 'my-2')
+    mealEl.setAttribute('data-id', meal.id)
+    mealEl.innerHTML = `
+    <div class="card-body">
+      <div class="d-flex align-items-center justify-content-between">
+        <h4 class="mx-1">${meal.name}</h4>
+        <div class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5">
+          ${meal.calories}
+        </div>
+        <button class="delete btn btn-danger btn-sm mx-2">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+    </div>
+  </div>
+    `
+    mealsEl.appendChild(mealEl)
+  }
+
+  _displayNewWorkout(workout) {
+    const workoutsEl = document.getElementById('workout-items')
+    const workoutEl = document.createElement('div')
+    workoutEl.classList.add('card', 'my-2')
+    workoutEl.setAttribute('data-id', workout.id)
+    workoutEl.innerHTML = `
+    <div class="card-body">
+      <div class="d-flex align-items-center justify-content-between">
+        <h4 class="mx-1">${workout.name}</h4>
+        <div class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5">
+          ${workout.calories}
+        </div>
+        <button class="delete btn btn-danger btn-sm mx-2">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+    </div>
+  </div>
+    `
+    workoutsEl.appendChild(workoutEl)
   }
 
   _render() {
@@ -121,10 +157,7 @@ class App {
 
     document
       .getElementById('meal-form')
-
-      // bind to App and not to the form where the click event is attached to
       .addEventListener('submit', this._newItem.bind(this, 'meal'))
-    //.addEventListener('submit', this._newMeal)
 
     document
       .getElementById('workout-form')
@@ -132,13 +165,9 @@ class App {
   }
 
   _newItem(type, e) {
-    console.log(type, e)
     e.preventDefault()
-
     const name = document.getElementById(`${type}-name`)
     const calories = document.getElementById(`${type}-calories`)
-
-    // Validate inputs
 
     if (name.value === '' || calories.value === '') {
       alert('Please fill in all fields')
@@ -148,16 +177,11 @@ class App {
     if (type === 'meal') {
       const meal = new Meal(name.value, +calories.value)
       this._tracker.addMeal(meal)
-    } else {
+    }
+    if (type === 'workout') {
       const workout = new Workout(name.value, +calories.value)
       this._tracker.addWorkout(workout)
     }
-
-    name.value = ''
-    calories.value = ''
-
-    const meal = new Meal(name.value, +calories.value)
-    this._tracker.addMeal(meal)
 
     name.value = ''
     calories.value = ''
