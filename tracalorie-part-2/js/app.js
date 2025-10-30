c07b17da41dbdcb9141516098b5ce8da3a4d2af4
Patrick <pfkellogg@gmail.com>
2025-10-30 11:54:34 -0500

    remove  meal and workout

--- tracalorie-part-2/js/app.js
+++ tracalorie-part-2/js/app.js
@@ -27,6 +27,30 @@ class CalorieTracker {
     this._render()
   }
 
+  removeMeal(id) {
+    const index = this._meals.findIndex(meal => meal.id === id)
+
+    if (index !== -1) {
+      const meal = this._meals[index]
+      this._totalCalories -= meal.calories
+      this._meals.splice(index, 1)
+      this._render()
+    }
+  }
+
+  removeWorkout(id) {
+    const index = this._workouts.findIndex(workout => workout.id === id)
+
+    if (index !== -1) {
+      const workout = this._workouts[index]
+      this._totalCalories += workout.calories
+      this._workouts.splice(index, 1)
+      this._render()
+    }
+  }
+
+  // Private Methods //
+
   _displayCaloriesTotal() {
     const totalCaloriesEl = document.getElementById('calories-total')
     totalCaloriesEl.innerHTML = this._totalCalories
@@ -162,6 +186,14 @@ class App {
     document
       .getElementById('workout-form')
       .addEventListener('submit', this._newItem.bind(this, 'workout'))
+
+    document
+      .getElementById('meal-items')
+      .addEventListener('click', this._removeItem.bind(this, 'meal'))
+
+    document
+      .getElementById('workout-items')
+      .addEventListener('click', this._removeItem.bind(this, 'workout'))
   }
 
   _newItem(type, e) {
@@ -190,6 +222,40 @@ class App {
     const bsCollapse = new bootstrap.Collapse(collapseItem, {
       toggle: true
     })
+  }
+
+  _removeItem(type, e) {
+    if (
+      e.target.classList.contains('delete') ||
+      e.target.classList.contains('fa-xmark')
+    ) {
+      if (confirm('Are you sure?')) {
+        console.log('e', e)
+        console.log('e.target', e.target)
+        console.log('e.target.parentElement', e.target.parentElement)
+        console.log(
+          'e.target.parentElement.parentElement',
+          e.target.parentElement.parentElement
+        )
+        console.log(
+          'e.target.parentElement.parentElement.parentElement',
+          e.target.parentElement.parentElement.parentElement
+        )
+        console.log(
+          'e.target.parentElement.parentElement.parentElement.parentElement',
+          e.target.parentElement.parentElement.parentElement.parentElement
+        )
+      }
+
+      const id = e.target.closest('.card').getAttribute('data-id')
+      console.log('id', id)
+
+      type === 'meal'
+        ? this._tracker.removeMeal(id)
+        : this._tracker.removeWorkout(id)
+
+      e.target.closest('.card').remove()
+    }
   }
 }
 
