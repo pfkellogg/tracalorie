6a7581e4e1553ecd8ab7f2fb6d7ccad6be71c388
Patrick <pfkellogg@gmail.com>
2025-11-02 12:11:32 -0500

    set daily limit

--- tracalorie-part-2/js/app.js
+++ tracalorie-part-2/js/app.js
@@ -56,6 +56,12 @@ class CalorieTracker {
     this._render()
   }
 
+  setLimit(calories) {
+    this._calorieLimit = calories
+    this._displayCaloriesLimit()
+    this._render()
+  }
+
   // Private Methods //
 
   _displayCaloriesTotal() {
@@ -213,6 +219,10 @@ class App {
     document
       .getElementById('reset')
       .addEventListener('click', this._reset.bind(this))
+
+    document
+      .getElementById('limit-form')
+      .addEventListener('submit', this._setLimit.bind(this))
   }
 
   _newItem(type, e) {
@@ -295,6 +305,24 @@ class App {
     document.getElementById('workout-items').innerHTML = ''
     document.getElementById('filter-meals').value = ''
     document.getElementById('filter-workouts').value = ''
+  }
+
+  _setLimit(e) {
+    e.preventDefault()
+
+    const limit = document.getElementById('limit')
+
+    if (limit.value === '') {
+      alert('Please add a limit')
+      return
+    }
+
+    this._tracker.setLimit(+limit.value)
+    limit.value = ''
+
+    const modalEl = document.getElementById('limit-modal')
+    const bsModal = bootstrap.Modal.getInstance(modalEl)
+    bsModal.hide()
   }
 }
 
